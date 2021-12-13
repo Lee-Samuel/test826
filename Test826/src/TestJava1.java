2238ca7401995601c55cc0b1c6e8e607d5768791
Samuel Lee <Samuel Lee@DESKTOP-NC8HGID>
2021-12-13 19:22:58 +0900

    버전 4.0
    변경점

---
+++ Test826/src/TestJava1.java
@@ -0,0 +1,9 @@
+
+public class TestJava2 {
+	public static void main(String[] args) {
+		System.out.println("Hello GIT World");
+		System.out.println("Version : 2.0");
+		System.out.println("·Î±×ÀÎ µÊ!!");
+	}
+
+}
